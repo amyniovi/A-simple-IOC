ca7b59544efd58a95405d830b0ba452a67712ef2
amyniovi <amyniovi@gmail.com>
2016-11-15 21:08:32 +0000

    Clean up

--- ControllerFactory/MyControllerFactory.cs
+++ ControllerFactory/MyControllerFactory.cs
@@ -54,30 +54,7 @@ namespace ControllerFactory
 			throw new NotImplementedException();
 		}
 	}
-	}
-
-
-/*
-public class My_Controller_Factory : DefaultControllerFactory
-{
-	public override IController CreateController
-	(System.Web.Routing.RequestContext requestContext, string controllerName)
-	{
-		string controllername = requestContext.RouteData.Values
-		["controller"].ToString();
-		// Debug.WriteLine(string.Format("Controller Name : {0}", controllername));            
-		Type controllerType = Type.GetType(string.Format
-			// ("Custom_Controller_Factory.Controllers.{0}",controllername));
-			// typeof(Home);            
-	   IController controller = Activator.CreateInstance(controllerType) as IController;
-		return controller;
-	}
-	public override void ReleaseController(IController controller)
-	{
-		IDisposable dispose = controller as IDisposable; if (dispose != null)
-		{
-			dispose.Dispose();
-		}
-	}
 }
-*/
+
+
+
