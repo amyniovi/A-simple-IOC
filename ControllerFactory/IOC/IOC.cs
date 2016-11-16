0a4cfe2e1a74a9706d67196480a9031d0c689a26
amyniovi <amyniovi@gmail.com>
2016-11-16 20:11:45 +0000

    Changed IOC to create from resolve

--- ControllerFactory/IOC/IOC.cs
+++ ControllerFactory/IOC/IOC.cs
@@ -7,6 +7,23 @@ namespace ControllerFactory
 	{
 		internal static Dictionary<Type, object> Dependencies = new Dictionary<Type, object>();
 
+		internal static Dictionary<Type, Type> RobertFace = new Dictionary<Type, Type>();
+
+		public static void Bind2<T, U>()
+		{
+			//enter write lock
+			try
+			{
+				RobertFace.Add(typeof(T), typeof(U));
+
+			}
+			finally
+			{
+
+				//exit write lock
+			}
+		}
+
 		public static void Bind<T>(T solidType) where T:class 
 		{
 			//enter write lock
@@ -32,16 +49,15 @@ namespace ControllerFactory
 		}
 		//simplified version, just news up the solid type assumming it has a parameterless constructor
 		public static T Resolve<T>()
-
 		{
-			object registration = null;
+			Type registration;
 
-			if (Dependencies == null || Dependencies.Count == 0)
+			if (RobertFace == null || RobertFace.Count == 0)
 				throw new Exception("IOC Framework Missing Registrations, including a registration for : " + typeof(T));
 
-			Dependencies.TryGetValue(typeof(T), out registration);
-			return (T)registration;//returns null if trygetvalue is false, test this
+			RobertFace.TryGetValue(typeof(T), out registration);
 
+			return (T) Activator.CreateInstance(registration);
 		}
 
 	}
